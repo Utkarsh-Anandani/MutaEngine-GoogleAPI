b89ef9a4c8e9eb359202fc68153028ef2e2b9a42
Utkarsh-Anandani <utkarshanandani9@gmail.com>
2024-09-24 12:56:20 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
 
   const login = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:3000/login', {
+    const response = await fetch('https://muta-engine-google-api-backend.vercel.app/login', {
       method: 'POST',
       credentials: 'include',
       body: JSON.stringify({ email, password }),
