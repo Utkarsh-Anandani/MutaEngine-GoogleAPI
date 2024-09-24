3d1b63a8172a8f69a8a0ac7187096194e62587a3
Utkarsh-Anandani <utkarshanandani9@gmail.com>
2024-09-24 20:04:17 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,10 +5,10 @@ const Navbar = ({ token, userDetails }) => {
   return (
     <nav className='flex flex-row items-center justify-between px-8 py-4 bg-sky-500'>
       <div className='text-2xl font-semibold text-white'>COURSES</div>
-      <div className='flex flex-row items-center justify-between w-[20vw]'>
+      <div className='flex flex-row items-center justify-between w-[20vw] gap-5'>
         <div className='text-white text-xl font-semibold'>About</div>
         <div className='text-white text-xl font-semibold'>Contact</div>
-        {token ? <div>
+        {!token ? <div className='flex justify-between items-center gap-5'>
           <Link to={'/login'}><div className='text-white text-xl font-semibold'>Signin</div></Link>
           <Link to={'/signup'}><div className='text-white text-xl font-semibold'>Signup</div></Link>
         </div>
